92ce3a26a74429de5929b57e60f80998ae83f07c
socialdemocracy <haeltor80@gmail.com>
2026-08-02 21:04:15 -0600

    Add colour and tooltip lists for political parties

---
+++ out/html/data.js
@@ -0,0 +1,37 @@
+const colourList = [{
+        word: "CPC",
+        style: "color: #EE1C25; font-weight: bold;"
+   },
+    {
+        word: "Guomindang",
+        style: "color: #0028CC; font-weight: bold;"
+   },
+    {
+        word: "Kuomintang",
+        style: "color: #0028CC; font-weight: bold;"
+   },
+    {
+        word: "KMT",
+        style: "color: #0028CC; font-weight: bold;"
+   },
+];
+
+
+
+const tooltipList = [{
+    searchString: "Kuomintang",
+    explanationText: "<img src=img/kuomingtang.webp> Chungkuo Kuomintang </br> Zhōngguó Guómíndǎng </br> Chinese Nationalist Party"
+    },
+    {
+    searchString: "Guomindang",
+    explanationText: "<img src=img/kuomingtang.webp> Chungkuo Kuomintang </br> Zhōngguó Guómíndǎng </br> Chinese Nationalist Party"
+    },
+    {
+    searchString: "KMT",
+    explanationText: "<img src=img/kuomingtang.webp> Chungkuo Kuomintang </br> Zhōngguó Guómíndǎng </br> Chinese Nationalist Party"
+    },
+    {
+    searchString: "CPC",
+    explanationText: "<img src=img/cpc.webp> Zhōngguó Gòngchǎndǎng </br> Communist Party Of China"
+    },
+];
