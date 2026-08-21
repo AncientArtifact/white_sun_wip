e56cf2bed2f6a159a81fd1322b0c13b0262d42f8
AncientArtifact <ancientartifact99@gmail.com>
2026-08-21 15:01:25 +0800

    button hiding test

--- out/html/game.js
+++ out/html/game.js
@@ -301,6 +301,12 @@
       return bar;
   };
 
+const powerstruggle_tab = document.getElementById('powerstruggle_tab')
+  if (dendryEngine.state.qualities.sunyatsen_dead true) {
+        powerstruggle_tab.hidden = false;
+    } else {
+        powerstruggle_tab.hidden = true;
+    }
 
   window.justLoaded = true;
   window.statusTab = "status";
