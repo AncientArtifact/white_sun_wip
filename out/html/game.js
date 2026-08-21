34387e8a431351bce35807cc3ad9f8ab1aae8716
AncientArtifact <ancientartifact99@gmail.com>
2026-08-21 11:51:24 +0800

    Update game.js

--- out/html/game.js
+++ out/html/game.js
@@ -294,6 +294,12 @@ window.changeTab = function(newTab, tabId) {
         document.body.classList.add('dark-mode');
     }
     window.pinnedCardsDescription = "Advisor cards - actions are only usable once per 6 months.";
+     window.statusTab = "status";
+    window.updateSidebar();
+    window.statusTabRight = "status_right";
+    window.updateSidebarRight();
+  };
+
   };
 
 }());
